7f4412f41e9fea4e56b0770e202e8ddac2421735
Marcelo <goski.marcelo@gmail.com>
2021-03-28 12:13:48 -0300

    firebase hosting

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/components/Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <p className="Footer-title">merch</p>
+            <p className="Footer-checkout">Todos los derechos reservados</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/containers/Success.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import '../styles/components/Success.css';
+import Map from '../components/Map'
+import useGoogleAddress from '../hooks/useGoogleAddress'
+
+const Success = () => {
+    const {state} = useContext(AppContext)
+    const { buyer } = state
+    const location = useGoogleAddress(buyer[0].address)
+    return (
+        <div className="Succes">
+            <div className="Success-content">
+                <h2>{`${buyer.name}, Gracias por tu compra`} </h2>
+                <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+                <div className="Success-map">
+                    <Map data={location}/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Success;
